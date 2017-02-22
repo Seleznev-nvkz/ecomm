c09504e11fc3fe5e64957c8616c07058d40fe183
Ruslan <avc_russel@mail.ru>
2017-02-22 19:23:51 +0700

    check1

---
+++ myshop/appshop/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    slider = models.BooleanField()
+    small_photo = models.ImageField()
+    big_photo = models.ImageField()
+    in_stock = models.BooleanField()
+    on_main_page = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    number = models.PositiveSmallIntegerField(default=0)
+    unique_code = models.PositiveSmallIntegerField()
+    person_name = models.CharField(max_length=200)
+    person_email = models.EmailField()
+    person_phone = models.CharField(max_length=200)
+    notes = models.CharField(max_length=200)
+
+    STATUS_CHOICES = (
+        ('New', 'New'),
+        ('In process', 'In process'),
+        ('Ready to deliver', 'Ready to deliver'),
+        ('Delivering', 'Delivering'),
+        ('Delivered', 'Delivered'),
+        ('Suspended', 'Suspended'),
+        ('Cancelled', 'Cancelled')
+    )
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='New')
+    comment = models.CharField(max_length=200)
+
+
+class OrderedProduct(models.Model):
+    quantity = models.PositiveSmallIntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+
+
+
+
---
+++ myshop/appshop/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Product, Category, OrderedProduct
+# Create your views here.
+
+
+def index(request):
+    slider = Product.objects.filter(slider=True)
+    product = Product.objects.filter(in_stock=True)
+    category = Category.objects.all()
+    context = {'products': product,
+               'sliders': slider,
+               'categories': category}
+    return render(request, 'index.html', context=context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def cart(request, id=1):
+    # product = Product.objects.get(id=id)
+    #
+    # cart = request.session.get('cart', {})
+    # if id == :
+    #     request.session[id] = quantity
+    # ordered_product = OrderedProduct.objects.create(product=product)
+    # context = {'carts': cart}
+    # return render(request, 'cart.html', context=context)
+    # cart = request.session.get('cart', {})
+    # cart[item_id] = quantity
+    # request.session['cart'] = cart
+    quantity = 1
+    my_buffer_cart = request.session.get('my_cart', {})
+    if id not in my_buffer_cart:
+        #request.session['my_cart'].items = (id:quantity)]
+        #request.session['my_cart'] = {id: quantity}
+        my_buffer_cart.update({id: quantity})
+    new = {}
+    for id, quantity in my_buffer_cart.items():
+        new[get_object_or_404(Product, id=id)] = quantity
+    context = {'news': new}
+    return render(request, 'cart.html', context=context)
+
+
+
+def item(request, id):
+    product = Product.objects.get(id=id)
+    category = Category.objects.all()
+    context = {'products': product,
+               'categories': category}
+    return render(request, 'item.html', context=context)
+
+
+def catalogue(request, cat):
+    category = Category.objects.get(short_description=cat)
+    product = Product.objects.filter(category=category)
+    category = Category.objects.all()
+    context = {'categories': category,
+               'products': product}
+    return render(request, 'catalogue.html', context=context)
+
+
